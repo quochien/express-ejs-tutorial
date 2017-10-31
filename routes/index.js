e29a4fcff28f5c695f57136c61c2ffc128deccc8
Hien Nguyen <quochien@gmail.com>
2017-10-31 20:37:37 +0700

    create patial views

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About' });
+});
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // res.send('respond with a resource');
+  res.render('user_list', { title: 'Users' });
+});
+router.get('/cool', function(req, res) {
+  res.send("You're so cool");
 });
 
 module.exports = router;
